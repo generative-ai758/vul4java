34f475637af69857ed097f36ad1759e07f8cf648
Leow Wee Sheng <leow.weesheng@ncs.com.sg>
2023-05-16 22:47:05 +0800

    vul4j-4

---
+++ VUL4J-4/camel-core/src/main/java/org/apache/camel/model/PollEnrichDefinition.java
@@ -0,0 +1,260 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.apache.camel.CamelContextAware;
+import org.apache.camel.Endpoint;
+import org.apache.camel.Processor;
+import org.apache.camel.processor.PollEnricher;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.apache.camel.processor.aggregate.AggregationStrategyBeanAdapter;
+import org.apache.camel.spi.Metadata;
+import org.apache.camel.spi.RouteContext;
+import org.apache.camel.util.ObjectHelper;
+
+/**
+ * Enriches messages with data polled from a secondary resource
+ *
+ * @see org.apache.camel.processor.Enricher
+ */
+@Metadata(label = "eip,transformation")
+@XmlRootElement(name = "pollEnrich")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PollEnrichDefinition extends NoOutputDefinition<PollEnrichDefinition> implements EndpointRequiredDefinition {
+    @XmlAttribute(name = "uri")
+    private String resourceUri;
+    // TODO: For Camel 3.0 we should remove this ref attribute as you can do that in the uri, by prefixing with ref:
+    @XmlAttribute(name = "ref")
+    @Deprecated
+    private String resourceRef;
+    @XmlAttribute @Metadata(defaultValue = "-1")
+    private Long timeout;
+    @XmlAttribute(name = "strategyRef")
+    private String aggregationStrategyRef;
+    @XmlAttribute(name = "strategyMethodName")
+    private String aggregationStrategyMethodName;
+    @XmlAttribute(name = "strategyMethodAllowNull")
+    private Boolean aggregationStrategyMethodAllowNull;
+    @XmlAttribute
+    private Boolean aggregateOnException;
+    @XmlTransient
+    private AggregationStrategy aggregationStrategy;
+
+    public PollEnrichDefinition() {
+    }
+
+    public PollEnrichDefinition(AggregationStrategy aggregationStrategy, String resourceUri, long timeout) {
+        this.aggregationStrategy = aggregationStrategy;
+        this.resourceUri = resourceUri;
+        this.timeout = timeout;
+    }
+
+    @Override
+    public String toString() {
+        return "PollEnrich[" + description() + " " + aggregationStrategy + "]";
+    }
+    
+    protected String description() {
+        return FromDefinition.description(getResourceUri(), getResourceRef(), (Endpoint) null);
+    }
+
+    @Override
+    public String getLabel() {
+        return "pollEnrich[" + description() + "]";
+    }
+
+    @Override
+    public String getEndpointUri() {
+        if (resourceUri != null) {
+            return resourceUri;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public Processor createProcessor(RouteContext routeContext) throws Exception {
+        if (ObjectHelper.isEmpty(resourceUri) && ObjectHelper.isEmpty(resourceRef)) {
+            throw new IllegalArgumentException("Either uri or ref must be provided for resource endpoint");
+        }
+
+        // lookup endpoint
+        Endpoint endpoint;
+        if (resourceUri != null) {
+            endpoint = routeContext.resolveEndpoint(resourceUri);
+        } else {
+            endpoint = routeContext.resolveEndpoint(null, resourceRef);
+        }
+
+        PollEnricher enricher;
+        if (timeout != null) {
+            enricher = new PollEnricher(null, endpoint.createPollingConsumer(), timeout);
+        } else {
+            // if no timeout then we should block, and there use a negative timeout
+            enricher = new PollEnricher(null, endpoint.createPollingConsumer(), -1);
+        }
+
+        AggregationStrategy strategy = createAggregationStrategy(routeContext);
+        if (strategy == null) {
+            enricher.setDefaultAggregationStrategy();
+        } else {
+            enricher.setAggregationStrategy(strategy);
+        }
+        if (getAggregateOnException() != null) {
+            enricher.setAggregateOnException(getAggregateOnException());
+        }
+
+        return enricher;
+    }
+
+    private AggregationStrategy createAggregationStrategy(RouteContext routeContext) {
+        AggregationStrategy strategy = getAggregationStrategy();
+        if (strategy == null && aggregationStrategyRef != null) {
+            Object aggStrategy = routeContext.lookup(aggregationStrategyRef, Object.class);
+            if (aggStrategy instanceof AggregationStrategy) {
+                strategy = (AggregationStrategy) aggStrategy;
+            } else if (aggStrategy != null) {
+                AggregationStrategyBeanAdapter adapter = new AggregationStrategyBeanAdapter(aggStrategy, getAggregationStrategyMethodName());
+                if (getAggregationStrategyMethodAllowNull() != null) {
+                    adapter.setAllowNullNewExchange(getAggregationStrategyMethodAllowNull());
+                    adapter.setAllowNullOldExchange(getAggregationStrategyMethodAllowNull());
+                }
+                strategy = adapter;
+            } else {
+                throw new IllegalArgumentException("Cannot find AggregationStrategy in Registry with name: " + aggregationStrategyRef);
+            }
+        }
+
+        if (strategy != null && strategy instanceof CamelContextAware) {
+            ((CamelContextAware) strategy).setCamelContext(routeContext.getCamelContext());
+        }
+
+        return strategy;
+    }
+
+    public String getResourceUri() {
+        return resourceUri;
+    }
+
+    /**
+     * The endpoint uri for the external service to poll enrich from. You must use either uri or ref.
+     */
+    public void setResourceUri(String resourceUri) {
+        this.resourceUri = resourceUri;
+    }
+
+    public String getResourceRef() {
+        return resourceRef;
+    }
+
+    /**
+     * Refers to the endpoint for the external service to poll enrich from. You must use either uri or ref.
+     *
+     * @deprecated use uri with ref:uri instead
+     */
+    @Deprecated
+    public void setResourceRef(String resourceRef) {
+        this.resourceRef = resourceRef;
+    }
+
+    public Long getTimeout() {
+        return timeout;
+    }
+
+    /**
+     * Timeout in millis when polling from the external service.
+     * <p/>
+     * The timeout has influence about the poll enrich behavior. It basically operations in three different modes:
+     * <ul>
+     *     <li>negative value - Waits until a message is available and then returns it. Warning that this method could block indefinitely if no messages are available.</li>
+     *     <li>0 - Attempts to receive a message exchange immediately without waiting and returning <tt>null</tt> if a message exchange is not available yet.</li>
+     *     <li>positive value - Attempts to receive a message exchange, waiting up to the given timeout to expire if a message is not yet available. Returns <tt>null</tt> if timed out</li>
+     * </ul>
+     * The default value is -1 and therefore the method could block indefinitely, and therefore its recommended to use a timeout value
+     */
+    public void setTimeout(Long timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getAggregationStrategyRef() {
+        return aggregationStrategyRef;
+    }
+
+    /**
+     * Refers to an AggregationStrategy to be used to merge the reply from the external service, into a single outgoing message.
+     * By default Camel will use the reply from the external service as outgoing message.
+     */
+    public void setAggregationStrategyRef(String aggregationStrategyRef) {
+        this.aggregationStrategyRef = aggregationStrategyRef;
+    }
+
+    public String getAggregationStrategyMethodName() {
+        return aggregationStrategyMethodName;
+    }
+
+    /**
+     * This option can be used to explicit declare the method name to use, when using POJOs as the AggregationStrategy.
+     */
+    public void setAggregationStrategyMethodName(String aggregationStrategyMethodName) {
+        this.aggregationStrategyMethodName = aggregationStrategyMethodName;
+    }
+
+    public Boolean getAggregationStrategyMethodAllowNull() {
+        return aggregationStrategyMethodAllowNull;
+    }
+
+    /**
+     * If this option is false then the aggregate method is not used if there was no data to enrich.
+     * If this option is true then null values is used as the oldExchange (when no data to enrich),
+     * when using POJOs as the AggregationStrategy.
+     */
+    public void setAggregationStrategyMethodAllowNull(Boolean aggregationStrategyMethodAllowNull) {
+        this.aggregationStrategyMethodAllowNull = aggregationStrategyMethodAllowNull;
+    }
+
+    public AggregationStrategy getAggregationStrategy() {
+        return aggregationStrategy;
+    }
+
+    /**
+     * Sets the AggregationStrategy to be used to merge the reply from the external service, into a single outgoing message.
+     * By default Camel will use the reply from the external service as outgoing message.
+     */
+    public void setAggregationStrategy(AggregationStrategy aggregationStrategy) {
+        this.aggregationStrategy = aggregationStrategy;
+    }
+
+    public Boolean getAggregateOnException() {
+        return aggregateOnException;
+    }
+
+    /**
+     * If this option is false then the aggregate method is not used if there was an exception thrown while trying
+     * to retrieve the data to enrich from the resource. Setting this option to true allows end users to control what
+     * to do if there was an exception in the aggregate method. For example to suppress the exception
+     * or set a custom message body etc.
+     */
+    public void setAggregateOnException(Boolean aggregateOnException) {
+        this.aggregateOnException = aggregateOnException;
+    }
+}
---
+++ VUL4J-4/camel-core/src/main/java/org/apache/camel/model/rest/VerbDefinition.java
@@ -0,0 +1,321 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.camel.model.rest;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElements;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.apache.camel.model.OptionalIdentifiedDefinition;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.model.ToDefinition;
+import org.apache.camel.spi.Metadata;
+
+/**
+ * Rest command
+ */
+@Metadata(label = "rest")
+@XmlRootElement(name = "verb")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class VerbDefinition extends OptionalIdentifiedDefinition<VerbDefinition> {
+
+    @XmlAttribute
+    private String method;
+
+    @XmlAttribute
+    private String uri;
+
+    @XmlAttribute
+    private String consumes;
+
+    @XmlAttribute
+    private String produces;
+
+    @XmlAttribute @Metadata(defaultValue = "auto")
+    private RestBindingMode bindingMode;
+
+    @XmlAttribute
+    private Boolean skipBindingOnErrorCode;
+
+    @XmlAttribute
+    private Boolean enableCORS;
+
+    @XmlAttribute
+    private String type;
+
+    @XmlAttribute
+    private String outType;
+
+    // used by XML DSL to either select a <to> or <route>
+    // so we need to use the common type OptionalIdentifiedDefinition
+    @XmlElements({
+            @XmlElement(required = false, name = "to", type = ToDefinition.class),
+            @XmlElement(required = false, name = "route", type = RouteDefinition.class)}
+    )
+    private OptionalIdentifiedDefinition<?> toOrRoute;
+
+    // the Java DSL uses the to or route definition directory
+    @XmlTransient
+    private ToDefinition to;
+    @XmlTransient
+    private RouteDefinition route;
+    @XmlTransient
+    private RestDefinition rest;
+
+    @Override
+    public String getLabel() {
+        if (method != null) {
+            return method;
+        } else {
+            return "verb";
+        }
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * The HTTP verb such as GET or POST
+     */
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    /**
+     * Uri template of this REST service such as /{id}.
+     */
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getConsumes() {
+        return consumes;
+    }
+
+    /**
+     * To define the content type what the REST service consumes (accept as input), such as application/xml or application/json.
+     * This option will override what may be configured on a parent level
+     */
+    public void setConsumes(String consumes) {
+        this.consumes = consumes;
+    }
+
+    public String getProduces() {
+        return produces;
+    }
+
+    /**
+     * To define the content type what the REST service produces (uses for output), such as application/xml or application/json
+     * This option will override what may be configured on a parent level
+     */
+    public void setProduces(String produces) {
+        this.produces = produces;
+    }
+
+    public RestBindingMode getBindingMode() {
+        return bindingMode;
+    }
+
+    /**
+     * Sets the binding mode to use.
+     * This option will override what may be configured on a parent level
+     * <p/>
+     * The default value is auto
+     */
+    public void setBindingMode(RestBindingMode bindingMode) {
+        this.bindingMode = bindingMode;
+    }
+
+    public Boolean getSkipBindingOnErrorCode() {
+        return skipBindingOnErrorCode;
+    }
+
+    /**
+     * Whether to skip binding on output if there is a custom HTTP error code header.
+     * This allows to build custom error messages that do not bind to json / xml etc, as success messages otherwise will do.
+     * This option will override what may be configured on a parent level
+     */
+    public void setSkipBindingOnErrorCode(Boolean skipBindingOnErrorCode) {
+        this.skipBindingOnErrorCode = skipBindingOnErrorCode;
+    }
+
+    public Boolean getEnableCORS() {
+        return enableCORS;
+    }
+
+    /**
+     * Whether to enable CORS headers in the HTTP response.
+     * This option will override what may be configured on a parent level
+     * <p/>
+     * The default value is false.
+     */
+    public void setEnableCORS(Boolean enableCORS) {
+        this.enableCORS = enableCORS;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the class name to use for binding from input to POJO for the incoming data
+     * This option will override what may be configured on a parent level
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOutType() {
+        return outType;
+    }
+
+    /**
+     * Sets the class name to use for binding from POJO to output for the outgoing data
+     * This option will override what may be configured on a parent level
+     */
+    public void setOutType(String outType) {
+        this.outType = outType;
+    }
+
+    public RestDefinition getRest() {
+        return rest;
+    }
+
+    public void setRest(RestDefinition rest) {
+        this.rest = rest;
+    }
+
+    public RouteDefinition getRoute() {
+        if (route != null) {
+            return route;
+        } else if (toOrRoute instanceof RouteDefinition) {
+            return (RouteDefinition) toOrRoute;
+        } else {
+            return null;
+        }
+    }
+
+    public void setRoute(RouteDefinition route) {
+        this.route = route;
+        this.toOrRoute = route;
+    }
+
+    public ToDefinition getTo() {
+        if (to != null) {
+            return to;
+        } else if (toOrRoute instanceof ToDefinition) {
+            return (ToDefinition) toOrRoute;
+        } else {
+            return null;
+        }
+    }
+
+    public void setTo(ToDefinition to) {
+        this.to = to;
+        this.toOrRoute = to;
+    }
+
+    public OptionalIdentifiedDefinition<?> getToOrRoute() {
+        return toOrRoute;
+    }
+
+    /**
+     * To route from this REST service to a Camel endpoint, or an inlined route
+     */
+    public void setToOrRoute(OptionalIdentifiedDefinition<?> toOrRoute) {
+        this.toOrRoute = toOrRoute;
+    }
+
+    // Fluent API
+    // -------------------------------------------------------------------------
+
+    public RestDefinition get() {
+        return rest.get();
+    }
+
+    public RestDefinition get(String uri) {
+        return rest.get(uri);
+    }
+
+    public RestDefinition post() {
+        return rest.post();
+    }
+
+    public RestDefinition post(String uri) {
+        return rest.post(uri);
+    }
+
+    public RestDefinition put() {
+        return rest.put();
+    }
+
+    public RestDefinition put(String uri) {
+        return rest.put(uri);
+    }
+
+    public RestDefinition delete() {
+        return rest.delete();
+    }
+
+    public RestDefinition delete(String uri) {
+        return rest.delete(uri);
+    }
+
+    public RestDefinition head() {
+        return rest.head();
+    }
+
+    public RestDefinition head(String uri) {
+        return rest.head(uri);
+    }
+
+    public RestDefinition verb(String verb) {
+        return rest.verb(verb);
+    }
+
+    public RestDefinition verb(String verb, String uri) {
+        return rest.verb(verb, uri);
+    }
+
+    public String asVerb() {
+        // we do not want the jaxb model to repeat itself, by outputting <get method="get">
+        // so we defer the verb from the instance type
+        if (this instanceof GetVerbDefinition) {
+            return "get";
+        } else if (this instanceof PostVerbDefinition) {
+            return "post";
+        } else if (this instanceof PutVerbDefinition) {
+            return "put";
+        } else if (this instanceof DeleteVerbDefinition) {
+            return "delete";
+        } else if (this instanceof HeadVerbDefinition) {
+            return "head";
+        } else {
+            return method;
+        }
+    }
+
+}
